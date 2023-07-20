f6d78f0d5e170f8511e7ea556e830c0cb490bd49
Arwinder Singh <76734033+Arwinder-Singh@users.noreply.github.com>
2023-07-20 16:01:43 +0530

    Update settings.py

--- kyp/settings.py
+++ kyp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$6$q!k6v5qr3%buivfm93mjyblbhv8y_bjrt70ekdekfpamn-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["knowyourprofessor.pythonanywhere.com"]
 
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
+# STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT= os.path.join(BASE_DIR,'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
